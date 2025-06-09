e08168bfa40bad46d49f028b28da2881fc46f6d3
ElifasPF (ElifasPF) <elifas.faustino@estudante.ifms.edu.br>
2025-06-09 12:38:05 -0300

    props e states

--- src/Body.tsx
+++
@@ -1,8 +0,0 @@
-function Body() {
-    return (
-        <body>
-            <h1>Body</h1>
-        </body>
-    )
-}
-export default Body
---
+++ src/Container.tsx
@@ -0,0 +1,60 @@
+//Props -> é utilizado para passar dados diferente para cada componemte em sua criação
+//States -> Estados, salvam estados de variáveis do react e quando esses estados muda.
+// Automaticamente o React entende que ele precisa atualizar a página para mostrar 
+// os novos valores do componente.
+/*
+function Container () {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    )
+}
+*/
+//Class
+/*import React from 'react'
+interface ContainerProps {
+    nome: string,
+}
+interface ContainerInfoState{
+    nome: string
+}
+
+//Container é filha de React.Component (herança -> herda os atributos)
+class Container extends React.Component<ContainerProps,ContainerInfoState>{
+    constructor(props: ContainerInfoState){
+        super(props)
+        this.state = {
+            nome: "Nome antes de Mudar"
+        }
+    }
+    mudar = ()=>{
+        this.setState({nome: "Nome depois de Mudar"})
+    }
+    render(): React.ReactNode {
+        return (
+            <>
+             <h1>{this.props.nome}</h1>
+             <p>{this.state.nome}</p>
+             <button onClick={this.mudar}>Mudar Estado</button>
+            </>           
+        )
+    }
+}*/
+interface ContainerProps {
+    nome: string
+}
+function Container (props:ContainerProps){
+    let texto = "Texto Antes de Mudar"
+    function mudar(){
+
+    }
+    return(
+        <>
+          <h1>{props.nome}</h1>
+          <p>{texto}</p>
+          <button onClick={mudar}>Mudar</button>
+        </>
+    )
+}
+export default Container
